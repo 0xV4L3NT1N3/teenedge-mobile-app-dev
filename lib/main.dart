150b6971e74ba90109fc13a6562b29274ea3eb93
0xV4L3NT1N3 <nicholaschn@protonmail.com>
2022-11-03 16:57:38 +0800

    Modified for week 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'services/quote_api.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,11 +8,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-  // variables holding quote and author data, set to a sample at first
-
-  String quote = 'Thought Vitamins';
-  String author = 'by 0xV4L3NT1N3';
 
   @override
   Widget build(BuildContext context) {
@@ -35,22 +29,16 @@ class _MyAppState extends State<MyApp> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(quote, style: TextStyle(fontSize: 30)),
+              Text("This is a quote", style: TextStyle(fontSize: 30)),
               SizedBox(height: 50),
-              Text(author, style: TextStyle(fontSize: 20, color: Colors.grey)),
+              Text("By it's author", style: TextStyle(fontSize: 20, color: Colors.grey)),
               SizedBox(height: 120),
               Center(
                 child: TextButton(
                   style: TextButton.styleFrom(backgroundColor: Colors.black54),
-                  onPressed: () async {
+                  onPressed: () {
 
-                    // call the getData function, and update quote variables
-
-                    Map data = await getData();
-                    setState(() {
-                      quote = data["content"];
-                      author = data["author"];
-                    });
+                    // our button function goes here
 
                   },
                   child: Row(
